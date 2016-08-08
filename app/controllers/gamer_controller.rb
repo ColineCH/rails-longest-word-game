e5e665b7434fe37dfa44ab27ba25c10d1b312e68
coline <colnatcoline@hotmail.com>
2016-08-08 19:04:14 +0200

    methode get

--- app/controllers/gamer_controller.rb
+++ app/controllers/gamer_controller.rb
@@ -4,15 +4,15 @@ class GamerController < ApplicationController
   def game
     @grid_size = params[:grid_size]
     @grid = Array.new(@grid_size.to_i) { ('A'..'Z').to_a[rand(26)] }
-    session[:grid] = @grid
+    # session[:grid] = @grid
   end
 
   def score
     @end_time = Time.now
     @answer = params[:answer]
     @start_time = Time.parse(params[:start_time])
-    # @grid = params[:grid].split("")
-    @grid = session[:grid]
+    @grid = params[:grid].split("")
+    # @grid = session[:grid]
     @result = run_game(@answer, @grid, @start_time, @end_time)
   end
 end
