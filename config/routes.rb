2941070501be0ff0bb04d000a494b0b71ce09f86
coline <colnatcoline@hotmail.com>
2016-08-08 18:47:28 +0200

    session hash

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'game', to: 'gamer#game'
 
   get 'score', to: 'gamer#score'
+  post 'score', to: 'gamer#score'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
